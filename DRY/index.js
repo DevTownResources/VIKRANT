c31a042fda6702110c560337ce31bed16b2adc59
Vikrant Raut <93037846+15VIKRANT@users.noreply.github.com>
2023-01-31 16:21:25 +0530

    added refactoring

---
+++ DRY/index.js
@@ -0,0 +1,37 @@
+// // let chips = ["balaji", "tata", "haldiram", "tango", "snack"];
+
+// // //non DRY Code
+
+// // // console.log(chips[0]);
+// // // console.log(chips[1]);
+// // // console.log(chips[2]);
+
+// // //DRY code
+
+// // for (let i = 0; i < chips.length; i++) {
+// //   console.log(chips[i]);
+// // }
+
+// let capital = ["delhi", "islamabad", "washingtonDC"];
+// let country = ["India", "Pakistan", "USA"];
+
+// //non dry
+
+// // console.log(country[0]);
+// // console.log(capital[0]);
+
+// // console.log(country[1]);
+// // console.log(capital[1]);
+// // console.log(country[2]);
+// // console.log(capital[2]);
+
+// //DRY
+
+// function CC(country, capital) {
+//   for (let i = 0; i < capital.length; i++) {
+//     console.log(country[i]);
+//     console.log(capital[i]);
+//   }
+// }
+
+// CC(country, capital);
