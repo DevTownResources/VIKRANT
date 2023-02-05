52753889ffef477b7a566e7fd41400d3ec0b0a08
Vikrant Raut <93037846+15VIKRANT@users.noreply.github.com>
2023-02-05 15:07:44 +0530

    added new two files
    logical assignment operators and for-of loop

---
+++ For-of Loop/index.js
@@ -0,0 +1,36 @@
+//for (variable of iterable) statement;
+//iteration
+//data structure //arrays // map //
+
+// let arr = [2, 3, 4, 5, 6];
+
+// for (let element of arr) {
+//   console.log(element);
+// }
+
+//new map
+
+// const iterable = new Map([
+//   ["a", 1],
+//   ["b", 2],
+//   ["c", 2],
+// ]);
+
+// for (let [key, value] of iterable) {
+//   console.log(key, " ", value);
+// }
+
+//String
+
+// let str = "devtown";
+// for (let element of str) {
+//   console.log(element);
+// }
+
+// function sum() {
+//   for (const element of arguments) {
+//     console.log(element);
+//   }
+// }
+
+// sum(1, 2, 3);
---
+++ Logical assignment operators/index.js
@@ -0,0 +1,38 @@
+//logical OR assignment
+
+//  x||=y
+
+// let title = "bangalore";
+// console.log(title);
+// title ||= "devtown"; // title || title="devtown"
+
+// console.log(title);
+
+//....................................................................................................
+
+// logical and assignment
+
+// let title = "a";
+// console.log(title);
+
+// title &&= "devtown";
+
+// // if (title) {
+// //   title = "devtown";
+// // }
+
+// console.log(title);
+
+//....................................................................................................
+
+//Nullish Coalescing logical assignment
+
+let title;
+
+title ??= "devtown";
+
+// if (title == "null" || title == "undefined") {
+//   title = "devtown";
+// }
+
+console.log(title);
