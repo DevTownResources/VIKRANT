e2833ce47b113b3f17a26491be93c7625eed8247
Vikrant Raut <93037846+15VIKRANT@users.noreply.github.com>
2023-02-09 14:15:05 +0530

    added forEach method in

---
+++ forEach/index.js
@@ -0,0 +1,20 @@
+//for Each
+
+let nums = [11, 12, 13, 14, 15, 16];
+
+// nums.forEach((element, index, array) => {
+//   console.log(element, index, array);
+// });
+
+//console.log(a);
+
+let sum = 0;
+
+// nums.forEach((n) => {
+//   sum = sum + n;
+// });
+// console.log(sum);
+
+nums.forEach((n) => {
+  console.log(3 * n);
+});
---
+++ forEach/map.js
@@ -0,0 +1,17 @@
+//for each map
+
+// let newMap = new Map();
+
+// newMap.set("a", "animal");
+// newMap.set("b", "ball");
+// newMap.set("c", "choclate");
+// // console.log(newMap);
+
+// newMap.forEach((values, key, map) => {
+//   console.log(values, key, map);
+// });
+
+let newSet = new Set(["animal", "port", "shipping", "telcom"]);
+newSet.forEach((n, key, set) => {
+  console.log(n, key, set);
+});
