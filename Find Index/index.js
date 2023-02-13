ed394c528a47daecedc5fefacd9cf64a765013e5
Vikrant Raut <93037846+15VIKRANT@users.noreply.github.com>
2023-02-13 15:20:10 +0530

    added all files

---
+++ Find Index/index.js
@@ -0,0 +1,6 @@
+//find index
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let output = arr.findIndex((element) => element >= 6);
+console.log(output);
---
+++ some and every/index.js
@@ -0,0 +1,29 @@
+//some and every
+
+//every (element, index, array. this.arg)
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let arr1 = [1, 3, 5, 7, 9];
+
+// const isOdd = (element) => element % 2 !== 0;
+
+// let output = arr1.every(isOdd);
+
+// console.log(output);
+
+let arr = [10, 20, 20, 40];
+
+let big = 5;
+let a = arr.every((e) => e > big);
+console.log(a);
+
+//some
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let arr1 = [1, 3, 5, 7, 9];
+
+// const isOdd = (element) => element % 2 !== 0;
+
+// let output = arr.some(isOdd);
+
+// console.log(output);
