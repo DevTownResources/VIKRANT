5cc386904029a29884485246b8db69b2fffb5a25
Vikrant Raut <93037846+15VIKRANT@users.noreply.github.com>
2023-02-16 13:06:20 +0530

    added numeric seperator

---
+++ numeric seperator/index.js
@@ -0,0 +1,22 @@
+// 10000034343
+// 100,000
+
+// let n = 1_00_000;
+// console.log(n);
+
+//BigInt
+// let num = 199_99_99932_325_4345555n;
+// console.log(num);
+
+//Binary
+
+let nibbles = 0b1011_0101_0101;
+// console.log(nibbles);
+
+// //octal and hexdecimal
+
+// let oct = 0o1234_5670;
+// console.log(oct);
+
+let hex = 0xd0_e0_f0;
+console.log(hex);
