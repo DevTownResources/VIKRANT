cb33519fba48f2bbad2435cfb2e8f3d5a569f22e
Vikrant Raut <93037846+15VIKRANT@users.noreply.github.com>
2023-02-16 17:40:33 +0530

    added Big INt
    Added Big Int

---
+++ BigInt/index.js
@@ -0,0 +1,19 @@
+// let a = 10000000000;
+// let b = BigInt(34325254564565566666666666666);
+
+// // console.log(b);
+
+// console.log(Number.MAX_SAFE_INTEGER);
+
+// let n = BigInt(Number.MAX_SAFE_INTEGER) + 2n;
+// console.log(n);
+
+console.log(4n / 2n);
+
+console.log(4n === 4);
+
+let arr = [5, 4, 7, 5, 3, -1n];
+arr.sort();
+console.log(arr);
+
+console.log(0n || 6);
