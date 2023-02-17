e5bfdff884d0c96aeae49e726f13f9dc118b979a
Vikrant Raut <93037846+15VIKRANT@users.noreply.github.com>
2023-02-17 15:54:25 +0530

    added files date method

---
+++ Date/index.js
@@ -0,0 +1,20 @@
+// let date = Date();
+// console.log(typeof date);
+
+// let date = new Date();
+// console.log(date);
+
+//static methods
+// 1 jan 1970 00:00:00
+// Date.now();
+
+// let start = Date.now();
+// console.log(start);
+
+//Date.parse()
+
+// let time = Date.parse("17 Feb 2023 00:00:00 GMT");
+// console.log(time);
+
+let birth = new Date("November 16, 1998  06:11:40");
+console.log(birth.getDay());
