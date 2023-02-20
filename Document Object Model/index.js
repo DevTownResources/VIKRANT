96e81248cb98bf32fd258c7ed37c251b8225d858
Vikrant Raut <93037846+15VIKRANT@users.noreply.github.com>
2023-02-20 18:06:02 +0530

    DOM I and II added
    DOM I and II added

---
+++ Document Object Model/index.js
@@ -0,0 +1,38 @@
+// // let obj = {
+// //   a: "devtown",
+// //   p: "devtownp",
+// // };
+
+// // console.log(obj.a);
+
+// // let a = document.querySelector("div");
+// // console.log(a.innerHTML);
+
+// document.body.style.background = "yellow";
+
+//Window
+//document //Browser
+
+//...............................select tAGs
+// let anchor = document.querySelectorAll("a");
+
+// let anchor = document.getElementById("aone");
+// console.log(anchor);
+
+//.......................................creating elements
+
+// let div = document.createElement("div");
+// div.innerHTML = "Devtown";
+
+// let head = document.createElement("h1");
+// head.innerHTML = "Dev";
+
+// div.append(head);
+
+// document.body.append(div);
+
+//..............................delete any element
+
+// let head = document.querySelector("h5");
+// console.log(head);
+// head.remove();
