bc6c0c80ea0702d1d33e2599671881420b299562
Vikrant Raut <93037846+15VIKRANT@users.noreply.github.com>
2023-02-21 22:49:47 +0530

    added DOMIII

---
+++ Style Attribute Class/index.js
@@ -0,0 +1,40 @@
+// let D = document.querySelectorAll("#devtown");
+
+// D.forEach((e) => {
+//   e.textContent = "google";
+// });
+// console.log(D);
+
+//modify
+//attributes
+//hasAttribute
+//getAttribute
+//setAttribute
+//removeAttribute
+
+// let image = document.querySelector("img");
+// console.log(image);
+
+// let output_has = image.hasAttribute("alt");
+// console.log(output_has);
+
+// let output_get = image.getAttribute("src");
+// console.log(output_get);
+
+// image.setAttribute("src", "https://www.flipkart.com");
+
+// image.removeAttribute("src");
+
+//style
+
+// let image = document.querySelector("img");
+// image.setAttribute("style", "height:50px");
+// image.style.width = "100px";
+// image.style.height = "100px";
+// image.style.border = "1px solid red";
+
+//classname
+
+let div = document.querySelector("div");
+console.log(div);
+div.className = "devtown";
